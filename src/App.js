e80afdf192adcf5394558d50d375601032c84be8
Vijay <rebalvijay930@gmail.com>
2025-02-07 14:35:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1 className='text-3xl font-bold text-red-700'>Hello Every One</h1>
+    </div>
+  );
+}
+
+export default App;
